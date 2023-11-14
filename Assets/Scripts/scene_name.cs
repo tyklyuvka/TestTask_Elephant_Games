430b28b0a8a21c4642e5693f52948688aabfa621
tyklyuvka <milliliza.tyk@gmail.com>
2023-11-14 09:23:33 +0300

    feat: add scenes and manipulating them
    Создание 2 сцен в проекте в папке Scenes (названия произвольные и логичные: одна сцена первая, другая — вторая).
    Кнопка позволяет переходить по сценам.

---
+++ Assets/Scripts/scene_name.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scene_name
+{
+    public const string FIRST_SCENE = "first_scene";
+    public const string SECOND_SCENE = "second_scene";
+}
